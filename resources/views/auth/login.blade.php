b1ffc228cd9a06d45de40cc81f310c30f4237c15
bona-098 <pencengirm@gmail.com>
2022-08-30 12:52:08 +0800

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Template</title>
+    <title>Login - Saa</title>
     <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -24,17 +24,27 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group">
-                            <label for="email" value="email" >Email</label>
-                            <input type="email" name="email" id="email" class="form-control"
-                                placeholder="email@example.com">
-                        </div>
                         <div class="form-group mb-4">
-                            <label for="password" value="password" >Password</label>
-                            <input type="password" name="password" id="password" class="form-control"
+                            <label for="email" :value="__('Email')" >Email</label>
+                            
+                            <input type="email" 
+                            name="email" 
+                            id="email" 
+                            class="form-control"
+                            placeholder="email@example.com">
+                        </div>
+
+                        <div class="form-group mb-4">
+                            <label for="password" :value="__('Password')"  >Password</label>
+                            
+                            <input type="password" 
+                                    name="password" 
+                                    id="password"
+                                    required autocomplete="current-password"
+                                    class="form-control"
                                 placeholder="enter your passsword">
                         </div>
-                        <input name="login" id="login" class="btn btn-block login-btn" type="button"
+                        <input name="login" id="login" class="btn btn-block login-btn" type="submit"
                             value="Login">
                     </form>
                     <a href="#!" class="forgot-password-link">Lupa Password?</a>
